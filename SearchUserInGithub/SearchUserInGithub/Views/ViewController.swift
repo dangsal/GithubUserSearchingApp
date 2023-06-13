4eacf2f38624edf596e2cfe841a18a92a446de84
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-06-14 02:23:31 +0900

    [FEAT] 돋보기 추가

--- SearchUserInGithub/SearchUserInGithub/Views/ViewController.swift
+++ SearchUserInGithub/SearchUserInGithub/Views/ViewController.swift
@@ -24,6 +24,10 @@ final class ViewController: UIViewController {
         $0.placeholder = TextLiterals.userSearchTextField
         $0.addPadding()
     }
+    private let searchImageView: UIImageView = UIImageView(image: ImageLiterals.searchImage).then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.tintColor = .gray
+    }
     
     // MARK: - property
     
@@ -42,6 +46,12 @@ final class ViewController: UIViewController {
         self.userSearchTextField.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: SizeLiterals.leadingTrailingPadding).isActive = true
         self.userSearchTextField.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -SizeLiterals.leadingTrailingPadding).isActive = true
         self.userSearchTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        self.userSearchTextField.addSubview(self.searchImageView)
+        self.searchImageView.trailingAnchor.constraint(equalTo: self.userSearchTextField.trailingAnchor, constant: -10).isActive = true
+        self.searchImageView.centerYAnchor.constraint(equalTo: self.userSearchTextField.centerYAnchor).isActive = true
+        self.searchImageView.widthAnchor.constraint(equalToConstant: 24).isActive = true
+        self.searchImageView.heightAnchor.constraint(equalToConstant: 24).isActive = true
     }
     
 }
