5830e648ec8770b2dca29f06b7d4f30c18ac8a9c
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-09-28 22:42:25 +0900

    [ADD] 기본 셋팅

---
+++ GithubUserSearch/GithubUserSearch/Presentation/Scene/SearchUser/ViewController/SearchUserViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchUserViewController.swift
+//  GithubUserSearch
+//
+//  Created by 이성호 on 2023/09/28.
+//
+
+import UIKit
+
+final class SearchUserViewController: UIViewController {
+    
+    // MARK: - life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
