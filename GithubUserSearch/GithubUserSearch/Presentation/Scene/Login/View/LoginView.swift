5440dc683109705868e6b44ce3637bc487ac37b1
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-09-29 00:24:31 +0900

    [FEAT] loginVutton 생성

--- GithubUserSearch/GithubUserSearch/Presentation/Scene/Login/View/LoginView.swift
+++ GithubUserSearch/GithubUserSearch/Presentation/Scene/Login/View/LoginView.swift
@@ -7,15 +7,57 @@
 
 import UIKit
 
+import SnapKit
+
 final class LoginView: UIView {
+    
+    // MARK: - ui component
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("깃허브로 로그인하기", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .black
+        button.layer.cornerRadius = SizeLiteral.buttonCornerRadiusValue
+        button.layer.masksToBounds = true
+        return button
+    }()
+    
+    // MARK: - property
     
     // MARK: - init
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.setupLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - func
+    
+    private func setupLayout() {
+        self.addSubview(self.loginButton)
+        self.loginButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(SizeLiteral.leadingTrailingPadding)
+            $0.bottom.equalToSuperview().inset(SizeLiteral.bottomPadding)
+            $0.height.equalTo(60)
+        }
+    }
 }
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct LoginViewPreview: PreviewProvider{
+    static var previews: some View {
+        UIViewPreview {
+            let view = LoginView()
+            return view
+        }.previewLayout(.sizeThatFits)
+    }
+}
+#endif
---
+++ GithubUserSearch/GithubUserSearch/Util/Literal/SizeLiteral.swift
@@ -0,0 +1,18 @@
+//
+//  SizeLiteral.swift
+//  GithubUserSearch
+//
+//  Created by 이성호 on 2023/09/29.
+//
+
+import Foundation
+
+enum SizeLiteral {
+    
+    // MARK: - button
+    static let buttonCornerRadiusValue: CGFloat = 8
+    
+    // MARK: - padding
+    static let leadingTrailingPadding: CGFloat = 20
+    static let bottomPadding: CGFloat = 20
+}
