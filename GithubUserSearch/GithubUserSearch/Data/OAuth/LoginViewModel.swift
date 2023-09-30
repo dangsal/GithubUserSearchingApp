7a45bb088f5952fd1c5c6b6459b25380668c04bb
Hoya <sholee0508@naver.com>
2023-09-30 22:11:19 +0900

    [FEAT] LoginViewModel 생성

---
+++ GithubUserSearch/GithubUserSearch/Data/OAuth/LoginViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewModel.swift
+//  GithubUserSearch
+//
+//  Created by LeeSungHo on 9/30/23.
+//
+
+import Foundation
+
+final class LoginViewModel {
+    
+    // MARK: - property
+    
+    // MARK: - init
+    init() {
+        
+    }
+    
+    // MARK: - func
+}
